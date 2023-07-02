b6b480c5452b72afeaa611bdace28ef668c6d662
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-02 20:02:56 +0530

    header done with weird background

--- trello-appwrite-clone/components/Header.tsx
+++ trello-appwrite-clone/components/Header.tsx
@@ -7,6 +7,25 @@ const Header = () => {
     return (
         <header>
             <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+
+                <div
+                    className="
+                    absolute
+                    top-0
+                    left-0
+                    w-full
+                    h-96
+                    bg-gradient-to-br
+                    from-pink-400
+                    to-[#0055D1]
+                    rounded-md
+                    filter
+                    blur-3xl
+                    opacity-50
+                    -z-50
+                    "
+                />
+
                 <Image
                     src="/../public/trelloLogo.png"
                     alt="Trello Logo"
@@ -25,7 +44,7 @@ const Header = () => {
                     <Avatar name="Esha T" round size="50" color="#0055D1" />
                 </div>
             </div>
-            <div className="flex items-center justify-center px-5 md:py-5">
+            <div className="flex items-center justify-center py-2 px-5 md:py-5">
                 <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
                     <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
                     GPT is summerizing your tasks for the day...
