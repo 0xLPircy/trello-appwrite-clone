50fbd6d1da259cbdadac7e2409ce60e4e2cec72c
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-03 14:27:22 +0530

    implementing appwrite cloud with env variable

--- trello-appwrite-clone/appwrite.ts
+++ trello-appwrite-clone/appwrite.ts
@@ -1,7 +1,11 @@
 import { Client, Account, ID, Databases, Storage } from 'appwrite';
 
-const client = new Client();
-
-client
+const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('64a19abcbf3dac6683c2');
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, ID, databases, storage };
