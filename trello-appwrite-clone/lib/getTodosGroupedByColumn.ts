815643d08d4b1ed7041171f07a7cdc2f8001a629
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-06 22:07:10 +0530

    implemented the todo ka data

--- trello-appwrite-clone/lib/getTodosGroupedByColumn.ts
+++ trello-appwrite-clone/lib/getTodosGroupedByColumn.ts
@@ -6,5 +6,28 @@ export const getTodosGroupedByColumn = async () => {
         process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
     );
 
-    console.log(data);
+    // console.log(data);
+
+    const todos = data.documents;
+    const columns = todos.reduce((acc, todo) => { //apparently acc racks up as we map through reduce and each gives a todo
+        if (!acc.get(todo.status)) {
+            acc.set(todo.status, {
+                id: todo.status,
+                todos: []
+            })
+        }
+
+        acc.get(todo.status)!.todos.push({
+            $id: todo.$id,
+            $createdAt: todo.$createdAt,
+            title: todo.title,
+            status: todo.status,
+            // get image only if it exists
+            ...(todo.image && { image: JSON.parse(todo.image) })
+        })
+
+        return acc;
+    }, new Map<TypedColumn, Column>());
+
+    console.log(columns)
 };
