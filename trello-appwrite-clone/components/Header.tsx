033cbd62f07faab0f6629982e6518ff21354dd63
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-10 18:31:07 +0530

    added some more gpt connection, incomp

--- trello-appwrite-clone/components/Header.tsx
+++ trello-appwrite-clone/components/Header.tsx
@@ -3,12 +3,30 @@ import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
+import { useEffect, useState } from "react";
 
 const Header = () => {
-    const [searchString, setSearchString] = useBoardStore((state) => [
+    const [board, searchString, setSearchString] = useBoardStore((state) => [
+        state.board,
         state.searchString,
         state.setSearchString,
-    ])
+    ]);
+
+    const [loading, setLoading] = useState<boolean>(false)
+    const [suggestion, setSuggestion] = useState<string>("")
+
+    useEffect(() => {
+        if (board.columns.size === 0) return;
+        setLoading(true);
+
+        const fetchSuggestionFunc = async () => {
+            const suggestion = await fetchSuggestion(board);
+            setSuggestion(suggestion);
+            setLoading(false);
+        };
+
+        fetchSuggestionFunc();
+    }, [board])
 
     return (
         <header>
---
+++ trello-appwrite-clone/lib/formatTodosForAI.ts
@@ -0,0 +1,8 @@
+const formatTodosForAi = (board: Board) => {
+    const todos = Array.from(board.columns.entries());
+
+    const flatArray = todos.reduce((map, [key, value]) => {
+        map[key] = value.todos;
+        return map;
+    }, {} as { [key in TypedColumn]: Todo[] });
+}
