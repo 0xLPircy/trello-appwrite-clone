85c409498b74153437a6533574c4d07bceb68d25
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-12 23:58:31 +0530

    gpt loading but not showing

--- trello-appwrite-clone/components/Header.tsx
+++ trello-appwrite-clone/components/Header.tsx
@@ -74,8 +74,12 @@ const Header = () => {
             </div>
             <div className="flex items-center justify-center py-2 px-5 md:py-5">
                 <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-                    <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
-                    GPT is summerizing your tasks for the day...
+                    <UserCircleIcon className={`inline-block h-10 w-10 text-[#0055D1] mr-1
+                    ${loading && "animate-spin"}
+                    `} />
+                    {suggestion && !loading
+                        ? suggestion
+                        : "GPT is summerizing your tasks for the day..."}
                 </p>
             </div>
         </header>
