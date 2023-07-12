c0391349af8112d9907140e9a8ae087f24e182e9
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-12 23:35:21 +0530

    formattodosforai

--- trello-appwrite-clone/lib/formatTodosForAI.ts
+++ trello-appwrite-clone/lib/formatTodosForAI.ts
@@ -5,4 +5,12 @@ const formatTodosForAi = (board: Board) => {
         map[key] = value.todos;
         return map;
     }, {} as { [key in TypedColumn]: Todo[] });
+
+    const flatArrayCounted = Object.entries(flatArray).reduce(
+        (map, [key, value]) => {
+            map[key as TypedColumn] = value.length;
+            return map;
+        },
+        {} as { [key in TypedColumn]: number }
+    )
 }
